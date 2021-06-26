ea4a8ec1558cd5652128ffa6634241002a0a97a3
GeekyMan007 <albert.shibu0998@gmail.com>
2021-06-26 23:01:36 +0530

    web client added

--- src/main/java/com/ust/person/controller/PersonController.java
+++ src/main/java/com/ust/person/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.ust.person.controller;
 
 import com.ust.person.exception.UserAlreadyExistsException;
+import com.ust.person.exception.UserNotFoundException;
 import com.ust.person.model.Person;
 import com.ust.person.service.PersonService;
 import io.swagger.annotations.Api;
@@ -8,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 @RestController
@@ -29,9 +29,24 @@ public class PersonController {
         }
     }
 
-    @PutMapping("/manipulate")
-    public ResponseEntity<?> manipulateData(@Valid @PathVariable() String id){
-        return null;
+    @PutMapping("/manipulate/delete")
+    public ResponseEntity<?> deleteData(@Valid @PathVariable() Long id){
+        try{
+            service.deletePerson(id);
+            return new ResponseEntity<String>("Deleted", HttpStatus.OK);
+        }catch (UserNotFoundException ue){
+            return new ResponseEntity<String>("User Not Found", HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PutMapping("/manipulate/update")
+    public ResponseEntity<?> updateData(@Valid @PathVariable() Long id){
+        try{
+            service.updatePerson(id);
+            return new ResponseEntity<String>("Updated", HttpStatus.OK);
+        }catch (UserNotFoundException ue){
+            return new ResponseEntity<String>("User Not Found", HttpStatus.FORBIDDEN);
+        }
     }
 }
 
--- src/main/java/com/ust/person/model/Address.java
+++ src/main/java/com/ust/person/model/Address.java
@@ -5,7 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 @Embeddable
 public class Address {
 
